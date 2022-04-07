995d05feba3483edd86141fb50086831f21cccbf
Alex Romanenko <githubalexrom@gmail.com>
2022-04-07 14:50:14 -0500

    Commit 4: Iimproved tests + impl fix + ReadMe update
    Improved tests allowed to find a bug in previous implementation. bug fixed

--- src/main/java/com/alexromanenko/PrimeNumberGeneratorImpl.java
+++ src/main/java/com/alexromanenko/PrimeNumberGeneratorImpl.java
@@ -13,7 +13,7 @@ public class PrimeNumberGeneratorImpl implements PrimeNumberGenerator{
     @Override
     public boolean isPrime(int value) {
 
-        if(value < 1)
+        if(value <= 1)
             return false;
 
         for(int i = 2; i <= Math.sqrt(value); i++){
--- src/test/java/PrimeNumberGeneratorTests.java
+++ src/test/java/PrimeNumberGeneratorTests.java
@@ -14,21 +14,23 @@ public class PrimeNumberGeneratorTests {
 
     @Test
     public void testIsPrimeForTrueWithPrimeNumber(){
-        int realPrimeNumber = 3;
-        Assert.assertTrue(underTest.isPrime(realPrimeNumber));
+        // this array can be initialized outside this method
+        int[] primeNumbersArray = new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
+        for(int num : primeNumbersArray)
+        Assert.assertEquals("Value in the array with the failed assertion: " + num,true, underTest.isPrime(num));
     }
 
     @Test
     public void testIsPrimeForFalseWithNotPrimeNumber(){
-        int notPrimeNumber = 4;
-        Assert.assertFalse(underTest.isPrime(notPrimeNumber));
+        int[] notPrimeNumbersArray = new int[]{4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,28,30,32};
+        for(int num : notPrimeNumbersArray)
+            Assert.assertEquals("Value in the array with the failed assertion: " + num,false, underTest.isPrime(num));
     }
 
     @Test
     public void testIsPrimeForFalseWithZeroOrNegativeNumber(){
-        int zeroNumber = 0;
-        int negativeNumber = -1;
-        Assert.assertFalse(underTest.isPrime(zeroNumber));
-        Assert.assertFalse(underTest.isPrime(negativeNumber));
+        int[] notPrimeNumbersArray = new int[]{0,1,-8,-9,-10,-12,-2,-3,-5,-7,-11,-13,-17,-19};
+        for(int num : notPrimeNumbersArray)
+            Assert.assertEquals("Value in the array with the failed assertion: " + num,false, underTest.isPrime(num));
     }
 }
